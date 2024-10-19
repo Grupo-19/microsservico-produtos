48ed3a84d80eda6ab5ef69625574410eb22fe278
Paula Cristina Gomes <grupofiaptechchallenge@gmail.com>
2024-10-19 12:19:32 -0300

    config git

---
+++ microsservico-produtos/src/test/java/com/br/microsservico_produtos/MicrosservicoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.microsservico_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
